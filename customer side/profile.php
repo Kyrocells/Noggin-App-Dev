295a7c1b0393670bded0a9062eb495ac1daa987a
hize <155721619+hizeazazel@users.noreply.github.com>
2024-07-06 13:49:53 +0800

    added profile.php

---
+++ customer side/profile.php
@@ -0,0 +1,44 @@
+<div class="container profile_container">
+    <div class="edit_profile">
+        <h2 class="edit_profile_title">Edit Profile</h2>
+        <form>
+        <!-- first and last name -->
+        <div class="input-group mt-4">
+            <input type="text" class="form-control" placeholder="First Name" aria-label="First Name">
+            <input type="text" class="form-control" placeholder="Last Name" aria-label="Last Name">
+        </div>
+        <!-- username -->
+        <div class="form-group mt-4">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Enter username">
+        </div>
+        <!-- contact number -->
+        <div class="form-group mt-4">
+            <label for="exampleInputEmail1">Contact Number</label>
+            <input type="text" class="form-control" id="exampleInputContactNumber1" placeholder="Enter Contact Number">
+        </div>
+        <!-- email -->
+        <div class="form-group mt-4">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <!-- password -->
+        <div class="form-group mt-4">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <!-- birthday -->
+        <div class="form-group mt-4">
+            <label for="exampleInputEmail1">Birthday</label>
+            <input type="text" class="form-control" id="exampleInputBirthday1" placeholder="Enter Birthday">
+        </div>
+        <!-- upload image -->
+        <div class="form-group">
+            <label for="editImage">Image</label>
+            <input type="file" class="form-control" id="editImage" name="image">
+        </div>
+        <!-- confirm button -->
+        <button type="submit" class="confirm_button mt-4">Confirm</button>
+        </form>
+    </div>
+</div>
