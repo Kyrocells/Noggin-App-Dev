ea89d0044d88f16f605d80f3f63e548f31bf438d
Yugine Lloyd M. Federis <138594282+Yowjin@users.noreply.github.com>
2024-07-07 13:55:41 +0800

    updated

--- Admin side/videos_data.php
+++ Admin side/videos_data.php
@@ -1,21 +1,27 @@
 <?php
-
 require 'addVideo_db.php';
 
 header('Content-Type: application/json');
 
-
-$sql = "SELECT video_title, num_videos_available FROM videos";
-$result = $conn->query($sql);
+$sql = "SELECT DATE_FORMAT(r.start_date, '%Y-%m') AS month_year, SUM(v.num_videos_available) AS total_sold
+        FROM rented_videos r
+        JOIN videos v ON r.video_id = v.video_id
+        GROUP BY DATE_FORMAT(r.start_date, '%Y-%m')
+        ORDER BY DATE_FORMAT(r.start_date, '%Y-%m')";
 
 $data = array();
-while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
+
+if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    $result->free();
+} else {
+    $data['error'] = 'Query failed';
 }
 
 $conn->close();
 
-
 echo json_encode($data);
-
 ?>
+
